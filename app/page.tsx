458cfc867d64ee556245eb91bf760593c18efdf5
Kush <jangidkush1234@gmail.com>
2024-05-25 23:40:14 +0530

    backend created

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
 import axios from "axios";
+import Signup from "./signup/page";
 
 async function getUserDetails() {
-  await new Promise((r)=>setTimeout(r,5000))
-  const response = await axios.get("https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details")
+
+  const response = await axios.get("http://localhost:3000/api/user")
 	return response.data;
 }
 
@@ -13,6 +14,7 @@ export default async function Home() {
     <div className="flex flex-col justify-center h-screen">
         <div className="flex justify-center">
             <div className="border p-8 rounded">
+
                 <div>
                     Name: {userData?.name}
                 </div>
